18bd27973469be2805e9f44e2e43899db7ea76e1
Joshua Suskin <jsuskin85@gmail.com>
2019-05-12 08:14:13 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,10 +1,8 @@
 def alphabetize(arr)
   alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  i = 0
   arr.sort_by do |str|
-    alph.index(str[i])
-    if alph.index(str[i]) == alph.index(arr[])
-      
+    str.split("").collect do |char|
+      alph.index(char)
     end
   end
 end
