7189700220801d4029c51fb2ca1bc1b0de47294b
Joshua Suskin <jsuskin85@gmail.com>
2019-05-12 08:00:36 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,7 @@ def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |str|
     alphabet.index(str[0])
-    if arr[arr.index(str) - 1].sta
+    if arr[arr.index(str) - 1].starts_with?(str[0]) && alphabet.index(arr[arr.index(str) - 1][1]) > alphabet.index(str[1])
+      
   end
 end
