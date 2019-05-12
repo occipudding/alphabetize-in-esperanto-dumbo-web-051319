b6a8e434dff6368ed65188b3b53524254009ca11
Joshua Suskin <jsuskin85@gmail.com>
2019-05-12 08:02:40 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,6 +3,7 @@ def alphabetize(arr)
   arr.sort_by do |str|
     alphabet.index(str[0])
     if arr[arr.index(str) - 1].starts_with?(str[0]) && alphabet.index(arr[arr.index(str) - 1][1]) > alphabet.index(str[1])
-      
+      arr[arr.index(str)], arr[arr.index(str) - 1] = arr[arr.index(str) - 1], arr[arr.index(str)]
+    end
   end
 end
