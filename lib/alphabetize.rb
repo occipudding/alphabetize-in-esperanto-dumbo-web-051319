0978db1f8d6fc388745b5e94d2c0fe86295cb798
Joshua Suskin <jsuskin85@gmail.com>
2019-05-12 08:03:34 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,8 +2,6 @@ def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |str|
     alphabet.index(str[0])
-    if arr[arr.index(str) - 1].starts_with?(str[0]) && alphabet.index(arr[arr.index(str) - 1][1]) > alphabet.index(str[1])
-      arr[arr.index(str)], arr[arr.index(str) - 1] = arr[arr.index(str) - 1], arr[arr.index(str)]
-    end
+    
   end
 end
